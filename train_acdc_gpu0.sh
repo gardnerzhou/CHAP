98fb89ec9b5b85ea3a9e2f9e18f59650ba56891b
gardnerzhou <31401830+gardnerzhou@users.noreply.github.com>
2025-05-22 21:37:57 +0800

    Delete train_acdc_gpu0.sh

--- train_acdc_gpu0.sh
+++
@@ -1,8 +0,0 @@
-python train_DS_dist_diff_2D.py  --gpu 0 --projectdim 16 --w_dist 0.01 --w_diff 1.0 --subexp run1 && \
-python train_DS_dist_diff_2D.py  --gpu 0 --projectdim 16 --w_dist 0.01 --w_diff 0.5 --subexp run2 && \
-python train_DS_dist_diff_2D.py  --gpu 0 --projectdim 16 --w_dist 0.01 --w_diff 0.3 --subexp run3 && \
-python train_DS_dist_diff_2D.py  --gpu 0 --projectdim 16 --w_dist 0.01 --w_diff 2.0 --subexp run4 
-
-
-
-
