92038208c8e0f09dba2626aafc1b1d4622d72012
gardnerzhou <31401830+gardnerzhou@users.noreply.github.com>
2025-05-22 21:38:05 +0800

    Delete train_acdc_gpu1.sh

--- train_acdc_gpu1.sh
+++
@@ -1,8 +0,0 @@
-python train_DS_dist_2D.py  --gpu 0 --projectdim 16 --subexp run1 && \
-python train_final_3D.py  --gpu 2 --w_f 0.5 --exp lamda_0.5 && \
-python train_final_3D.py  --gpu 2 --w_f 1.5 --exp lamda_1.5 && \
-python train_final_3D.py  --gpu 2 --w_f 3.0 --exp lamda_3.0 
-
-
-
-
